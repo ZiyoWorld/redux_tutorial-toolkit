bac2d7c56b2fbd67c36dbae85199a647d96f5758
Mirziyod_ms <sunatillayevmirziyod363@gmail.com>
2023-03-12 10:18:33 +0300

    thunk

--- src/components/posts/PostsList.js
+++ src/components/posts/PostsList.js
@@ -1,49 +1,44 @@
 import { useSelector, useDispatch } from "react-redux";
-import { postsDelete, selectAllPosts } from "../../features/postSlice";
-
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "../../features/postSlice";
+import { useEffect } from "react";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
-  const posts = useSelector(selectAllPosts);
-
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
   const dispatch = useDispatch();
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-      <div className="distance">
-        <button
-          className="ondelete"
-          onClick={() => dispatch(postsDelete(post.id))}
-        >
-          Delete
-        </button>
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-        {/* <button
-          className="ondelete"
-          onClick={() => dispatch(postUpdated(post.id))}
-        >
-          Update
-        </button> */}
-      </div>
-    </article>
-  ));
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
+
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
--- src/features/usersSlice.js
+++ src/features/usersSlice.js
@@ -1,15 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
 const initialState = [
-  { id: "0", name: "Mirziyod Sunatillayev" },
-  { id: "1", name: "Azizbek Ravshanov" },
-  { id: "2", name: "Isroil Ortiqov" },
+  { id: "0", name: "Muhammad Yusuf" },
+  { id: "1", name: "Umar ibn Hattob" },
+  { id: "3", name: "Mirziyod Sunatillayev" },
 ];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
